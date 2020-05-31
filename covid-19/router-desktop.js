6ba3372b87fee93c52a7aad66436406846bf9110
1N0T <correo.1n0t@gmail.com>
2020-05-31 21:25:04 +0200

    Delete router-desktop.js

--- covid-19/router-desktop.js
+++
@@ -1,9 +0,0 @@
-const router = new VueRouter({
-   mode: 'hash',
-   routes: [
-     { path: '/',           component: AnalisisIncidenciaDesktop },
-   ],
-   linkActiveClass:      "w3-green",  // active class for non-exact links.
-   linkExactActiveClass: "w3-orange"  // active class for exact links.
-})
- 
